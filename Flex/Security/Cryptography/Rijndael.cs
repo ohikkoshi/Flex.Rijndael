f1358a41a0b2696b301fc19dc06422bda811d05b
ohikkoshi <ohikkoshi@icloud.com>
2022-06-27 17:18:23 +0900

    Add Compiler Attribute

--- Flex/Security/Cryptography/Rijndael.cs
+++ Flex/Security/Cryptography/Rijndael.cs
@@ -1,38 +1,48 @@
-using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace Flex.Security.Cryptography
 {
-	public class Rijndael
+	public sealed class Rijndael
 	{
 		readonly ICryptoTransform encoder;
 		readonly ICryptoTransform decoder;
 
 
-		private Rijndael()
-		{
-		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <value></value>
+		Rijndael() { }
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="password"></param>
+		/// <param name="bit"></param>
 		public Rijndael(string password, int bit = 128)
 		{
-			Debug.Assert(!string.IsNullOrEmpty(password));
-
 			var crypt = new RijndaelManaged();
 			crypt.KeySize = bit;
 			crypt.BlockSize = bit;
 
-			byte[] utf8 = Encoding.UTF8.GetBytes(password);
-			byte[] key = ToArray(ref utf8, crypt.Key.Length);
-			byte[] iv = ToArray(ref utf8, crypt.IV.Length);
-
+			var utf8 = Encoding.UTF8.GetBytes(password);
+			var key = ToArray(ref utf8, crypt.Key.Length);
+			var iv = ToArray(ref utf8, crypt.IV.Length);
 			encoder = crypt.CreateEncryptor(key, iv);
 			decoder = crypt.CreateDecryptor(key, iv);
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="array"></param>
+		/// <param name="size"></param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		byte[] ToArray(ref byte[] array, int size)
 		{
 			var buff = new byte[size];
@@ -44,28 +54,38 @@ namespace Flex.Security.Cryptography
 			return buff;
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="str"></param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public string Encode(string str)
 		{
-			byte[] utf8 = Encoding.UTF8.GetBytes(str);
+			var utf8 = Encoding.UTF8.GetBytes(str);
 
 			using (var ms = new MemoryStream())
-			using (var cs = new CryptoStream(ms, encoder, CryptoStreamMode.Write))
-			{
+			using (var cs = new CryptoStream(ms, encoder, CryptoStreamMode.Write)) {
 				cs.Write(utf8, 0, utf8.Length);
 				cs.FlushFinalBlock();
 
-				return Convert.ToBase64String(ms.ToArray());
+				return System.Convert.ToBase64String(ms.ToArray());
 			}
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="str"></param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public string Decode(string str)
 		{
-			byte[] base64 = Convert.FromBase64String(str);
+			var base64 = System.Convert.FromBase64String(str);
 
 			using (var ms = new MemoryStream(base64))
 			using (var cs = new CryptoStream(ms, decoder, CryptoStreamMode.Read))
-			using (var stream = new StreamReader(cs, Encoding.UTF8))
-			{
+			using (var stream = new StreamReader(cs, Encoding.UTF8)) {
 				return stream.ReadToEnd();
 			}
 		}
